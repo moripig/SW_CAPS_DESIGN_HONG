4b97330f46e6e4136063e7e0073177ae610b2c57
Hong, Minwoo <ghdalsdn123@gmail.com>
2021-03-16 04:44:32 +0900

    개인 commit

---
+++ app/src/main/java/net/skhu/traveler/CalendarActivity.java
@@ -0,0 +1,80 @@
+package net.skhu.traveler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.DatePickerDialog;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    private TextView textViewLocation;
+    private TextView textView_DateStart;
+    private TextView textView_DateEnd;
+    private DatePickerDialog.OnDateSetListener callbackMethod;
+
+    private String[] items = {"1", "2", "3", "4", "5"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        textViewLocation = findViewById(R.id.textViewLocation);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,items);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        this.InitializeView();
+        this.InitializeListener();
+    }
+
+    public void InitializeView()
+    {
+        textView_DateStart = (TextView)findViewById(R.id.textView_dateStart);
+        textView_DateEnd = (TextView)findViewById(R.id.textView_dateEnd);
+    }
+
+    public void InitializeListener()
+    {
+        callbackMethod = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+            {
+                monthOfYear += 1;
+                textView_DateStart.setText(year + "년 " + monthOfYear + "월 " + dayOfMonth + "일");
+                textView_DateEnd.setText(year + "년 " + monthOfYear + "월 " + dayOfMonth + "일");
+            }
+        };
+    }
+
+    public void OnClickHandler(View view)
+    {
+        DatePickerDialog dialog = new DatePickerDialog(this, callbackMethod, 2021, 2, 14);
+
+        dialog.show();
+    }
+}
---
+++ app/src/main/java/net/skhu/traveler/MainActivity.java
@@ -0,0 +1,63 @@
+package net.skhu.traveler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button intent_btn1;
+    Button intent_btn2;
+    Button intent_btn3;
+    Button intent_btn4;
+
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_option, menu);
+        return true; }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        intent_btn1 = findViewById(R.id.intent_btn1);
+        intent_btn2 = findViewById(R.id.intent_btn2);
+        intent_btn3 = findViewById(R.id.intent_btn3);
+        intent_btn4 = findViewById(R.id.intent_btn4);
+
+        intent_btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent1 = new Intent(MainActivity.this, CalendarActivity.class);
+
+                startActivity(intent1);
+            }
+        });
+
+        intent_btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent2 = new Intent(MainActivity.this, MapActivity.class);
+
+                startActivity(intent2);
+            }
+        });
+
+        intent_btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent3 = new Intent(MainActivity.this, MapActivity.class);
+
+                startActivity(intent3);
+            }
+        });
+
+    }
+}
