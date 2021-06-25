b43aed8c72329960c49dbd751880b6c118841077
Jeong, Jeehyeok <jhj0462@gmail.com>
2021-06-25 22:08:04 +0900

    카카오 업데이트

---
+++ app/src/main/java/net/skhu/kakao/SessionCallback.java
@@ -0,0 +1,81 @@
+package net.skhu.kakao;
+
+import android.util.Log;
+
+import com.kakao.auth.ISessionCallback;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.usermgmt.response.model.Profile;
+import com.kakao.usermgmt.response.model.UserAccount;
+import com.kakao.util.OptionalBoolean;
+import com.kakao.util.exception.KakaoException;
+
+public class SessionCallback implements ISessionCallback {
+
+    // 로그인에 성공한 상태
+    @Override
+    public void onSessionOpened() {
+        requestMe();
+    }
+
+    // 로그인에 실패한 상태
+    @Override
+    public void onSessionOpenFailed(KakaoException exception) {
+        Log.e("SessionCallback :: ", "onSessionOpenFailed : " + exception.getMessage());
+    }
+
+    // 사용자 정보 요청
+    public void requestMe() {
+        UserManagement.getInstance()
+                .me(new MeV2ResponseCallback() {
+                    @Override
+                    public void onSessionClosed(ErrorResult errorResult) {
+                        Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
+                    }
+
+                    @Override
+                    public void onFailure(ErrorResult errorResult) {
+                        Log.e("KAKAO_API", "사용자 정보 요청 실패: " + errorResult);
+                    }
+
+                    @Override
+                    public void onSuccess(MeV2Response result) {
+                        Log.i("KAKAO_API", "사용자 아이디: " + result.getId());
+
+                        UserAccount kakaoAccount = result.getKakaoAccount();
+                        if (kakaoAccount != null) {
+
+                            // 이메일
+                            String email = kakaoAccount.getEmail();
+
+                            if (email != null) {
+                                Log.i("KAKAO_API", "email: " + email);
+
+                            } else if (kakaoAccount.emailNeedsAgreement() == OptionalBoolean.TRUE) {
+
+
+                            } else {
+
+                            }
+
+                            // 프로필
+                            Profile profile = kakaoAccount.getProfile();
+
+                            if (profile != null) {
+                                Log.d("KAKAO_API", "nickname: " + profile.getNickname());
+                                Log.d("KAKAO_API", "profile image: " + profile.getProfileImageUrl());
+                                Log.d("KAKAO_API", "thumbnail image: " + profile.getThumbnailImageUrl());
+
+                            } else if (kakaoAccount.profileNeedsAgreement() == OptionalBoolean.TRUE) {
+
+
+                            } else {
+
+                            }
+                        }
+                    }
+                });
+    }
+}
